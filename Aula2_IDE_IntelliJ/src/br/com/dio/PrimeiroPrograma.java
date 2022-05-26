295d0be89da100118f094ba4713a234aa27d29f1
surtarso <tarso.galvao@gmail.com>
2022-05-26 14:01:20 -0300

    vscode test

--- Aula2_IDE_IntelliJ/src/br/com/dio/PrimeiroPrograma.java
+++ Aula2_IDE_IntelliJ/src/br/com/dio/PrimeiroPrograma.java
@@ -7,6 +7,7 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
 
         System.out.println(gato);
+
 /*        int a = 5;
         int b = 3;
         System.out.println("Hello World! " + (a + b));*/
