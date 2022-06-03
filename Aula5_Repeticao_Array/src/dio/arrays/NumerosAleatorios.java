f98fe1ad3345158bbb1252807b8346521a7c6f2e
surtarso <tarso.galvao@gmail.com>
2022-06-03 07:59:46 -0300

    added studies

---
+++ Aula5_Repeticao_Array/src/dio/arrays/NumerosAleatorios.java
@@ -0,0 +1,29 @@
+package dio.arrays;
+
+import java.util.Random;
+
+public class NumerosAleatorios {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int[] numerosAleatorios = new int[20];
+
+        for(int i = 0; i < numerosAleatorios.length; i++) {
+            int numero = random.nextInt(100);
+            numerosAleatorios[i] = numero;
+        }
+        System.out.print("\nantecessor aleatorios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.print((numero - 1) + " ");
+        }
+        System.out.print("\n   aleatorios gerados: ");
+        for (int numero : numerosAleatorios) {
+            System.out.print(numero + " ");
+        }
+        System.out.print("\nsucessores aleatorios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.print((numero + 1) + " ");
+        }
+    }
+}
--- Aula5_Repeticao_Array/src/dio/arrays/OrdemInversa.java
+++ Aula5_Repeticao_Array/src/dio/arrays/OrdemInversa.java
@@ -4,8 +4,18 @@ import java.util.Scanner;
 
 public class OrdemInversa {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+//        Scanner scan = new Scanner(System.in);
+        System.out.print("normal: ");
+        int[] vetor = {-81, 5, 67, 234, -8, 777};
+        int count = 0;
 
-
+        while(count < vetor.length) {
+            System.out.print(vetor[count] + " ");
+            count++;
+        }
+        System.out.print(" inverso: ");
+        for (int i = (vetor.length - 1); i >= 0; i--) {
+            System.out.print(vetor[i] + " ");
+        }
     }
 }
